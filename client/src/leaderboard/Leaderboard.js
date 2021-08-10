950f91f38f716ba2ab08fc8d461ecb8d48185a3e
Seamus Riordan <seamus@riordan.io>
2021-08-10 12:44:31 -0500

    Move to grid layout

--- client/src/leaderboard/Leaderboard.js
+++ client/src/leaderboard/Leaderboard.js
@@ -8,15 +8,26 @@ export function Leaderboard({data}) {
         .sort((a, b) => b.correctWeeks - a.correctWeeks)
         .map((leader, index) =>
             <LeaderboardRow key={`leader-${index}`}
-                            testId={`leader-${index}`}
+                            data-testid={`leader-${index}`}
                             name={leader.name}
                             weeks={leader.correctWeeks}
                             picks={leader.correctPicks}/>
         );
+    const cellHeaderBaseClasses = "leaderboard__cell leaderboard__cell--no-right-border ";
+
     return <div key="grid-leaders" className="leaderboard__container">
-        <div className="grid__cell grid__cell--leader grid__cell--border-bottom leaderboard__row leaderboard__row--name">Leaders</div>
-        <div className="grid__cell grid__cell--leader grid__cell--border-bottom leaderboard__row leaderboard__row--numerical">Weeks Won</div>
-        <div className="grid__cell grid__cell--leader grid__cell--border-bottom leaderboard__row leaderboard__row--numerical">Total Correct</div>
+        <div className={`${cellHeaderBaseClasses} leaderboard__cell--name`}
+            data-testid="leaderboard-names-header">
+            Leaders
+        </div>
+        <div className={`${cellHeaderBaseClasses} leaderboard__cell--numerical`}
+            data-testid="leaderboard-weeks-header">
+            Weeks Won
+        </div>
+        <div className={`${cellHeaderBaseClasses} leaderboard__cell--numerical`}
+            data-testid="leaderboard-picks-header">
+            Total Correct
+        </div>
         {rows}
     </div>
 }
