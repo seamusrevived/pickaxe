a772e5b4ecd45ceb34bf38d8dee80baf7b44bb21
Seamus Riordan <seamus@riordan.io>
2021-08-10 18:08:51 -0500

    Add responsiveness to week buttons and put in flex container

--- client/src/ChangeWeek.js
+++ client/src/ChangeWeek.js
@@ -1,14 +1,23 @@
 import React from "react";
 import './ChangeWeek.css'
-import arrowForward from './arrow_forward_ios-24px.svg'
+import {ReactComponent as Arrow} from './arrow_forward_ios-24px.svg'
 
 const ChangeWeek = props => {
     const {week, forward, back} = props;
 
-    return <div className="change-week" >
-        <img src={arrowForward} id="change-week--back" className="change-week__element change-week__element--back" onClick={back} alt="Back"/>
-        <div id="change-week--week" className="change-week__element change-week__element--week">{`${week}`}</div>
-        <img src={arrowForward} id="change-week--forward" onClick={forward} className="change-week__element" alt="Next"/>
+    return <div className="change-week change-week__container">
+        <Arrow id="change-week--back"
+               className="change-week change-week__button change-week__button--back"
+               onClick={back}
+        />
+        <span id="change-week--week"
+             className="change-week change-week__week">
+            {`${week}`}
+        </span>
+        <Arrow id="change-week--forward"
+               onClick={forward}
+               className="change-week change-week__button"
+        />
     </div>
 }
 
